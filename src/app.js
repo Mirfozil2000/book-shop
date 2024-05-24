dd425fbe530f2c3090a1a5d7083925f7990cdb5a
Mirfozil <mirfoziln@gmail.com>
2024-05-24 17:37:04 +0500

    finish

--- src/app.js
+++ src/app.js
@@ -1,24 +1,28 @@
-'use strict'
-import './app.css';
+import { FavoritesView } from './views/favourites/favourites';
 import { MainView } from './views/main/main';
 
-
 class App {
-    routes = [
-        { path: '', view: MainView }
-    ];
-    appState = {
-        favourites: []
-    };
-    constructor() {
-        window.addEventListener('hashchange', this.route.bind(this))
-        this.route()
-    }
-    route() {
-        const view = this.routes.find(r => r.path === location.hash).view
-        this.currentView = new view(this.appState);
-        this.currentView.render();
-    }
+	routes = [
+		{path: "", view: MainView },
+		{path: "#favorites", view: FavoritesView },
+	];
+	appState = {
+		favourites: []
+	};
+
+	constructor() {
+		window.addEventListener('hashchange', this.route.bind(this));
+		this.route();
+	}
+
+	route() {
+		if (this.currentView) {
+			this.currentView.destroy();
+		}
+		const view = this.routes.find(r => r.path == location.hash).view;
+		this.currentView = new view(this.appState);
+		this.currentView.render();
+	}
 }
 
 new App();
--- src/common/view.js
+++ src/common/view.js
@@ -1,13 +1,16 @@
 export class AbstractView {
     constructor() {
-        this.app = document.getElementById('root')
+        this.app = document.getElementById('root');
     }
+
     setTitle(title) {
-        document.title = title
+        document.title = title;
     }
+
     render() {
         return;
     }
+
     destroy() {
         return;
     }
